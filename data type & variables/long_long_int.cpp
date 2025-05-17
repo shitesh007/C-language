dca5a70b8636d67138b034cbfdb3914be89f99a3
shitesh kumar <sitesh9334@gmail.com>
2025-05-17 16:31:31 +0530

    Add files via upload

---
+++ data type & variables/long_long_int.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long int huge = 123456789012345;
+    cout << "Long Long: " << huge << endl;
+    return 0;
+}
+// For very large integers, long long can hold 64-bit values
